07f06f6df494dc758a8f4ae8c9416edca1bdb2bb
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-02-29 01:07:21 +0300

    make functions easier to understand, rewrite name of variables and functions, add sclice method to Task 5, add semicolon at the end of the strings, rewrite Task 14 and Task 15, rewrite task 16

--- H-W-4/Normal_level.ts
+++ H-W-4/Normal_level.ts
@@ -1,9 +1,9 @@
 // [Task 1] Пользователь передает через переменную свою фамилию и имя. Выведите приветсвие `Привет <Имя> <Фамилия>` 5 раз используя для каждого варианта разный регистр
 console.log('Task 1:');
 {
-    let a: string = `Ilya Yarashevich;`;
+    let fullName: string = `Ilya Yarashevich;`;
     for (let i: number = 1; i <= 5; i++) {
-        (i % 2 == 0) ? console.log(`Привет ${a.toUpperCase()}`) : console.log(`Привет ${a.toLowerCase()}`)
+        console.log((i % 2 == 0) ? `Привет ${fullName.toUpperCase()}` : `Привет ${fullName.toLowerCase()}`);
     }
 }
 // [Task 2] Дана строка 'я учу typescript!'. Проверить, есть ли данное слово в строке: если есть - вывести позицию подстроки 'учу', если нет - вывести строку `Данной подстроки нет`
@@ -11,33 +11,31 @@ console.log('Task 2:');
 console.log('Task 2. Вариант 1:');
 {
     let str: string = "Я учу typescript!";
-    (~str.indexOf('учу')) ? console.log(`Позиция подстроки: ${str.indexOf('учу')}`) : console.log('Данной строки нет');
+    console.log((~str.indexOf('учу')) ? `Позиция подстроки: ${str.indexOf('учу')}` : 'Данной строки нет');
 }
 console.log('Task 2. Вариант 2:');
 {
     let str: string = "Я тест typescript!";
-    str.includes('учу') ? console.log(str.indexOf('учу')) : console.log('Данной строки нет');
-
+    console.log(str.includes('учу') ? `Позиция подстроки: ${str.indexOf('учу')}` : 'Данной строки нет');
 }
 //[Task 3] Дана произвольная строка. Реализуйте метод, который принимает позицию символа в строке. Если переданное число больше длины строки, то должно отобразиться сообщение: `Вы вышли за границу строки`
 console.log('Task 3:');
 {
-    let str1: any = "Random String";
-    
-    console.log(`Длина строки ${str1} =  ${str1.length} символов`);
-    function SymbolPosition(num: number) {
-        return (num > str1.length || num < 0) ? console.log(`Упс, Вы ввели ${num} и вышли за границу строки`) : console.log('Всё ок')
+    let str: string = "Random String";
+    console.log(`Длина строки ${str} = ${str.length} символов`);
+    function getPositionOfSymbol(posOfSymb: number): string {
+        return posOfSymb > str.length || posOfSymb < 0 ? `Упс, Вы ввели ${posOfSymb} и вышли за границу строки` : 'Всё ок';
     }
-    let test = SymbolPosition(0);
+    let position = console.log(getPositionOfSymbol(150));
 }
 //[Task 4] Дана произвольная строка. Реализуйте метод, который проверяет заканчивается ли предложение точкой или нет. 
 //Если оканчивается, вывести предложение - `Данное предложение закончено`, если нет, то `В конце предложения не хватает точки`
 console.log('Task 4:');
 {
-    function dotChecher(str: string) {
-        return str.endsWith(`.`) ? console.log('Данное предложение закончено') : console.log(`В конце предложения не хватает точки`)
+    function checkDotInString(str: string): string {
+        return str.endsWith(`.`) ? 'Данное предложение закончено' : `В конце предложения не хватает точки`;
     }
-    let cheker = dotChecher('Случайная строка.');
+    let stringForCheck = console.log(checkDotInString('Случайная строка.'));
 }
 //[Task 5] Дана строка 'я учу typescript!'. Вырежите из нее слово 'учу' и слово 'typescript' разными способами (через substring, slice).
 console.log('Task 5:');
@@ -45,12 +43,13 @@ console.log('Task 5:');
     let str: string = "я учу typescript!";
     console.log(str.substring(2, 5));
     console.log(str.slice(-15, -12));
+    console.log(str.slice(2, 5));
 }
 //[Task 6] Дана строка 'я учу typescript!'. С помощью метода split запишите каждое слово этой строки в отдельный элемент массива.
 console.log('Task 6:');
 {
-    let str: string = "я учу typescript!";
-    let str2 = str.split(" ");
+    let str1: string = "я учу typescript!";
+    let str2: string[] = str1.split(" ");
     console.log(str2);
 }
 //[Task 7] Дана строка '                я учу typescript!                '. Выведите строку, без начальных и пробелов в конце строки.
@@ -68,29 +67,29 @@ console.log('Task 8:');
 //[Task 9] Дана произвольное число. Реализовать метод проверяющий является ли это число целочисленным или нет. Если является - вывести его, если нет - округлить до целой части.
 console.log('Task 9:');
 {
-    function anynumber(num) {
-        return (Number.isInteger(num)) ? console.log(num) : console.log(Math.round(num))
+    function checkNumberIsInteger(num: number): number {
+        return Number.isInteger(num) ? num : Math.round(num);
     }
-    let s = anynumber(5.4);
+    let anyNumber = console.log(checkNumberIsInteger(5.4));
 }
 //[Task 10] Дана произвольное число с плавающей точкой. Реализовать методы округления данного числа до ближайшего меньшего, большего аргумента и ближайшего целого числа.
 console.log('Task 10:');
 {
-    function floatnumber1(float1: number): number {
-        return Math.floor(float1)
+    function getRoundNumberToSmallerOne(floatNum1: number): number {
+        return Math.floor(floatNum1);
     }
-    function floatnumber2(float2: number): number {
-        return Math.ceil(float2)
+    function getRoundNumberToLargerOne(floatNum2: number): number {
+        return Math.ceil(floatNum2);
     }
-    function floatnumber3(float3: number): number {
-        return Math.round(float3)
+    function getRoundNumberToIntegerOne(floatNum3: number): number {
+        return Math.round(floatNum3);
     }
-    let s1 = floatnumber1(5.6);
-    let s2 = floatnumber2(5.4);
-    let s3 = floatnumber3(5.5);
-    console.log(`Округляем до ближайшего меньшего ${Math.floor(s1)}`);
-    console.log(`Округляем до ближайшего большего ${Math.ceil(s2)}`);
-    console.log(`Округляем до ближайшего целого числа ${Math.round(s3)}`);
+    let anyFloatNumber1 = getRoundNumberToSmallerOne(5.6);
+    let anyFloatNumber2 = getRoundNumberToLargerOne(5.4);
+    let anyFloatNumber3 = getRoundNumberToIntegerOne(5.5);
+    console.log(`Округляем до ближайшего меньшего ${anyFloatNumber1}`);
+    console.log(`Округляем до ближайшего большего ${anyFloatNumber2}`);
+    console.log(`Округляем до ближайшего целого числа ${anyFloatNumber3}`);
 }
 //[Task 11] Дана произвольное число с плавающей точкой. Реализовать методы округления данного числа до ближайшего меньшего, большего аргумента и ближайшего целого числа.
 console.log('Task 11 is the same as Task 10:');
@@ -99,33 +98,25 @@ console.log('Task 11 is the same as Task 10:');
 //[Task 12] Дана произвольное число. Реализовать метод возводящий число в степень. Степень возведения указывается как параметр к методу.
 console.log('Task 12:');
 {
-    function power(anynumb: number, n: number): number {
-        return (Math.pow(anynumb, n))
+    function getNumberRaisedToPower(anyNum: number, power: number): number {
+        return Math.pow(anyNum, power);
     }
-    let pow = power(10, 2);
-    console.log(`Результат возведения в степень = ${pow}`);
+    let anyNumbers = console.log(`Результат возведения в степень = ${getNumberRaisedToPower(10, 2)}`);
 }
 //[Task 13] Дана произвольное число. Реализовать метод возводящий число в степень. Степень возведения указывается как параметр к методу.
 console.log('Task 13 is the same as Task 12:');
 {
 }
 //[Task 14] Реализовать метод получения рандомного целочисленного числа в заданном диапазоне. Диапазон задается аргументами фунции.
-console.log('Task 14:');
-{
-    function Rand(min1, max2) {
-        return Math.random() * (max2 - min1) + min1;
-    }
-    let int = Rand(100, 1000);
-    console.log(`Случайноe целочисленное число: ${Math.round(int)}`);
-}
 //[Task 15] Реализовать метод получения рандомного числа с плавающей точкой в заданном диапазоне. Диапазон задается аргументами фунции
-console.log('Task 15:');
+console.log('Task 14 and Task 15:');
+
+function getRandomNumber(minNum: number, maxNum: number): number {
+    return Math.random() * (maxNum - minNum) + minNum;
+}
 {
-    function Rand1(min3, max4) {
-        return Math.random() * (max4 - min3) + min3;
-    }
-    let fl = Rand1(1.5, 125.75);
-    console.log(`Случайноe число с плавающей точкой: ${fl}`);
+    let rangeOfNumbersForRoundToInt = console.log(`Случайное целочисленное число: ${Math.round(getRandomNumber(100, 1000))}`);
+    let rangeOfNumbersForGetFloat = console.log(`Случайноe число с плавающей точкой: ${getRandomNumber(100, 1000)}`);
 }
 //[Task 16] Выведите дату сегоднешнего дня в формате: "текущая дата: месяц/год/день. Текущее время часы:минуты:секунды.". Использовать только внутренние методы Date.
 console.log('Task 16:');
@@ -134,8 +125,11 @@ console.log('Task 16:');
     let month: any = date.getMonth() + 1,
         year: any = date.getFullYear(),
         day: any = date.getDate(),
-        time: any = date.toLocaleTimeString();
-    let formdate = month + "/" + year + "/" + day;
-    console.log(`Текущая дата: ${formdate}. Текущее время ${time}`);
+        hours: any = date.getUTCHours(),
+        minutes: any = date.getUTCMinutes(),
+        seconds: any = date.getUTCSeconds();
+    let formatedDate = month + "/" + year + "/" + day;
+    let formatedTime = hours + ":" + minutes + ":" + seconds;
+    console.log(`Текущая дата: ${formatedDate}. Текущее время ${formatedTime}`);
 }
 
