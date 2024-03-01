9c335ae07e3baccd01df788cab1e9640e536c18f
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-01 16:43:53 +0300

    add: home-work.

--- H-W-5/H-W-5.ts
+++ H-W-5/H-W-5.ts
@@ -29,18 +29,58 @@ console.log(`TASK_2`);
 + значение по*/
 console.log(`TASK_3`);
 {
-    function getSlicedString(anyStr: String, from: number, by: number): any {
-        let slicedString = `${anyStr}`;
-        let slicing = slicedString.slice(from, by);
-        return [slicedString, slicing];
+    function getSlicedString(anyStr: String, from: number, by: number): string {
+        return `${anyStr.slice(from, by)}`;
     }
 }
-    console.log(getSlicedString('Совершенно случайная строка', 2,5));
+console.log(getSlicedString('I love function in TypeScript!', 2, 6));
 /*------------------------------------------------------------[TASK_4]------------------------------------------------------------------------------------------------
 Написать функцию **getSumNumbers**, которая будет принимать число и вычислять сумму цифр из которых состоит число.
 > Для 2021 это будет 5.*/
-console.log(`TASK_3`);
+console.log(`TASK_4`);
 {
+    function getSumOfNumbers(numberInString: string): number {
+        let stringFromNumber: string = numberInString.toString();
+        let sum: number = 0;
+        for (let i: number = 0; i < stringFromNumber.length; i++) {
+            sum += +stringFromNumber.charAt(i);
+        }
+        return sum
+    }
+    console.log(getSumOfNumbers('99'));
+}
+/*------------------------------------------------------------[TASK_5]------------------------------------------------------------------------------------------------
+Написать функцию **`getSum`** которая принимает два целых числа a и b, которые могут быть положительными или отрицательными, найти сумму всех чисел между ними, включая их,
+и вернуть ее. Если два числа равны, верните a или b.
+    getSum(1, 0)   // 1 + 0 = 1
+    getSum(1, 2)   // 1 + 2 = 3
+    getSum(0, 1)  // 0 + 1 = 1
+    getSum(1, 1)   // 1 Since both are same
+    getSum(-1, 0) // -1 + 0 = -1
+    getSum(-1, 2)  // -1 + 0 + 1 + 2 = 2
+*/
+console.log(`TASK_5`);
+{
+    function GetSumOfNUmbersInRange(num1: number, num2: number): any {
+        let sum: number = 0;
+        if (num1 < num2 && num1 != num2) {
+            for (num1; num1 <= num2; num1++) {
+                sum += num1;
+            }
+            return sum
 
-    
+        }
+        else if (num1 > num2 && num1 != num2) {
+            for (num2; num2 <= num1; num2++) {
+                sum += num2;
+            }
+            return sum
+
+        }
+        else if (num1 == num2) {
+            return sum = num1; //поправить
+        }
+
+    }
+    console.log(GetSumOfNUmbersInRange(-1, -1));
 }
