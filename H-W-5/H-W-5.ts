066398f8b7666473ad60559f5576f89a06fa39ee
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-02 01:35:49 +0300

    modify: hw tasks

--- H-W-5/H-W-5.ts
+++ H-W-5/H-W-5.ts
@@ -33,7 +33,7 @@ console.log(`TASK_3`);
         return `${anyStr.slice(from, by)}`;
     }
 }
-console.log(getSlicedString('I love function in TypeScript!', 2, 6));
+console.log(getSlicedString('I love functions in TypeScript!', 2, 6));
 /*------------------------------------------------------------[TASK_4]------------------------------------------------------------------------------------------------
 Написать функцию **getSumNumbers**, которая будет принимать число и вычислять сумму цифр из которых состоит число.
 > Для 2021 это будет 5.*/
@@ -93,13 +93,89 @@ console.log(`TASK_5`);
 */
 console.log(`TASK_6`);
 {
-    let getBooleanFunc1Func2 = (bool: boolean, func1: Function, func2: Function) => (bool == true) ? func1('func1') : func2('func2');
+    let getBooleanFunc1Func2 = (bool: boolean, func1: Function, func2: Function) => (bool == false) ? func1() : func2();
 
-    let func1 = (fun1Name: string) => console.log(`${fun1Name}`);
+    let func1 = () => console.log(`func1`);
 
-    let func2 = (fun2name: string) => console.log(`${fun2name}`);
-    getBooleanFunc1Func2(false, func1, func2);
+    let func2 = () => console.log('func2');
+    getBooleanFunc1Func2(true, func1, func2);
 }
+{
+
+
+
+}
+/*------------------------------------------------------------[TASK_7]------------------------------------------------------------------------------------------------
+((a, b) => a+b)(5, 3); //a = 5, b = 3
+Напишите самовызыввающиеся функцию подсчет факторила числа: число должно передаваться как параметр функции
+*/
+console.log(`TASK_7`);
+{
+    (function (ourNumber2: number) {
+        let resultOffactorial: number = 1;
+        for (let i: number = 1; i <= ourNumber2; i++) {
+            resultOffactorial *= i;
+        }
+        console.log(resultOffactorial);
+    })(7);
+}
+/*------------------------------------------------------------[TASK_1.ADVANCED_LEVEL]------------------------------------------------------------------------------------------------
+Реализуйте функцию, который принимает 3 целочисленных значения a, b, c. Функция должна возвращать **true**, 
+если треугольник можно построить со сторонами заданной длины, и **false** в любом другом случае.
+*/
+console.log(`TASK_1.ADVANCED LEVEL`);
+{
+    function getThreeSidesOfTriangle(a: number, b: number, c: number): boolean {
+        return ((a + b > c) && (a + c > b) && (b + c > a) && (a + b != c) && (a + c != b) && (b + c != a) && (a > 0) && (b > 0) && (c > 0)) ? true : false
+    };
+    console.log(getThreeSidesOfTriangle(2, 2, 3));
+    console.log(getThreeSidesOfTriangle(-1, 2, 3));
+}
+/*------------------------------------------------------------[TASK_2.ADVANCED_LEVEL]------------------------------------------------------------------------------------------------
+Ваша задача - разбить плитку шоколада заданного размера n x m на маленькие квадраты. 
+Каждый квадрат имеет размер 1x1 и не может быть разбит. Реализуйте функцию, которая будет возвращать минимальное количество необходимых надломов.
+Например, если вам дается плитка шоколада размером 2 x 1, вы можете разделить ее на отдельные квадраты всего за один надлом, но для размера 3 x 1 вы должны сделать два надлома.
+Если входные данные недействительны, вы должны вернуть 0 (поскольку надломы не требуются, если у нас нет шоколада для разделения). Ввод всегда будет неотрицательным целым числом.
+*/
+console.log(`TASK_2.ADVANCED LEVEL`);
+
+function getAmountOfChocolateSpread(a: number, b: number, side: Function): void {
+    let aSize = side(a);
+    let bSize = side(b);
+    if (aSize == bSize) {
+        console.log(`Минимальное количество разломов: ${(aSize)}`)
+    }
+    if (aSize == 0 || bSize == 0) {
+        console.log(`Шоколада нет`)
+
+    }
+};
+getAmountOfChocolateSpread(3, 1, side);
+
+function side(n: number): number {
+    let iteration_num = 0;
+    for (n; n > 1; n--) {
+        iteration_num++;
+    }
+    return iteration_num
+}
+
+
+
+
+
+// function funM(m: number): number {
+//     let iteration_num1 = 0;
+//     if (m / 1 !== 1) {
+//         for (m; m > 1; m--) {
+//             iteration_num1++;
+//         }
+//     }
+
+//     return iteration_num1;
+// };
+
+
 
 
 
