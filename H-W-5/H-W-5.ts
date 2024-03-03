f3ad444185144926d8c5566e3490f906de5aed4f
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-04 00:54:49 +0300

    modify: task 3 advanced level, add: 2nd variant of resolving 5 task. Other fixes.

--- H-W-5/H-W-5.ts
+++ H-W-5/H-W-5.ts
@@ -1,3 +1,5 @@
+import { FunctionBody } from "typescript";
+
 /*------------------------------------------------------------[TASK_1]------------------------------------------------------------------------------------------------
 Написать функцию **getSum**, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем. 
 > Если передадим число 100 то, надо вычислить сумму чисел от 0 до 100 (должно получится 5050)*/
@@ -9,7 +11,7 @@ console.log(`TASK_1`);
             total_sum += i;
         };
         return total_sum;
-    }
+    };
     console.log(`Сумма чисел от 0 до введённого числа = ${calcSumFromZeroToNum(123)}`);
 }
 /*------------------------------------------------------------[TASK_2]------------------------------------------------------------------------------------------------
@@ -31,9 +33,10 @@ console.log(`TASK_3`);
 {
     function getSlicedString(anyStr: String, from: number, by: number): string {
         return `${anyStr.slice(from, by)}`;
-    }
+    };
+    console.log(getSlicedString('I love functions in TypeScript!', 2, 6));
 }
-console.log(getSlicedString('I love functions in TypeScript!', 2, 6));
+
 /*------------------------------------------------------------[TASK_4]------------------------------------------------------------------------------------------------
 Написать функцию **getSumNumbers**, которая будет принимать число и вычислять сумму цифр из которых состоит число.
 > Для 2021 это будет 5.*/
@@ -44,13 +47,14 @@ console.log(`TASK_4`);
         let sum: number = 0;
         for (let i: number = 0; i < stringFromNumber.length; i++) {
             sum += +stringFromNumber.charAt(i);
-        }
-        return sum
-    }
+        };
+        return sum;
+    };
     console.log(getSumOfNumbers('99'));
 }
 /*------------------------------------------------------------[TASK_5]------------------------------------------------------------------------------------------------
-Написать функцию **`getSum`** которая принимает два целых числа a и b, которые могут быть положительными или отрицательными, найти сумму всех чисел между ними, включая их,
+Написать функцию **`getSum`** которая принимает два целых числа a и b, которые могут быть положительными или отрицательными, 
+найти сумму всех чисел между ними, включая их,
 и вернуть ее. Если два числа равны, верните a или b.
     getSum(1, 0)   // 1 + 0 = 1
     getSum(1, 2)   // 1 + 2 = 3
@@ -59,31 +63,43 @@ console.log(`TASK_4`);
     getSum(-1, 0) // -1 + 0 = -1
     getSum(-1, 2)  // -1 + 0 + 1 + 2 = 2
 */
-console.log(`TASK_5`);
+console.log(`TASK_5.Вариант 1.`);
 {
     function GetSumOfNUmbersInRange(num1: number, num2: number): number {
         let sum: number = 0;
         if (num1 < num2 && num1 != num2) {
             for (num1; num1 <= num2; num1++) {
                 sum += num1;
-            }
-            return sum
-
+            };
+            return sum;
         }
         else if (num1 > num2 && num1 != num2) {
             for (num2; num2 <= num1; num2++) {
                 sum += num2;
-            }
-            return sum
-
+            };
+            return sum;
         }
         else if (num1 == num2) {
-
-        }
+        };
         return num1;
-    }
-    console.log(GetSumOfNUmbersInRange(1, 1));
+    };
+    console.log(GetSumOfNUmbersInRange(0, 5));
 }
+//Второй вариант задачи номер 5.
+console.log(`TASK_5.Вариант 2.`);
+{
+    function GetSumOfNUmbersInRange2(num3: number, num4: number): number {
+        if (num3 < num4) {
+            return ((2 * num3 + ((num4 + 1 - num3) - 1)) / 2) * (num4 + 1 - num3);
+        }
+        else if (num4 < num3) {
+            return ((2 * num4 + ((num3 + 1 - num4) - 1)) / 2) * (num3 + 1 - num4);
+        }
+        else { return num3 };
+    };
+    console.log(GetSumOfNUmbersInRange2(5, 0));
+}
+
 /*------------------------------------------------------------[TASK_6]------------------------------------------------------------------------------------------------
 Напишите функцию fooboo которая принимает в качестве аргумента три параметра:
 +булевое значение
@@ -93,17 +109,10 @@ console.log(`TASK_5`);
 */
 console.log(`TASK_6`);
 {
-    let getBooleanFunc1Func2 = (bool: boolean, func1: Function, func2: Function) => (bool == false) ? func1() : func2();
-
+    let getBooleanFunc1Func2 = (bool: boolean, func1: Function, func2: Function) => (bool) ? func1() : func2();
     let func1 = () => console.log(`func1`);
-
     let func2 = () => console.log('func2');
-    getBooleanFunc1Func2(true, func1, func2);
-}
-{
-
-
-
+    getBooleanFunc1Func2(false, func1, func2);
 }
 /*------------------------------------------------------------[TASK_7]------------------------------------------------------------------------------------------------
 ((a, b) => a+b)(5, 3); //a = 5, b = 3
@@ -115,7 +124,7 @@ console.log(`TASK_7`);
         let resultOffactorial: number = 1;
         for (let i: number = 1; i <= ourNumber2; i++) {
             resultOffactorial *= i;
-        }
+        };
         console.log(resultOffactorial);
     })(7);
 }
@@ -126,7 +135,7 @@ console.log(`TASK_7`);
 console.log(`TASK_1.ADVANCED LEVEL`);
 {
     function getThreeSidesOfTriangle(a: number, b: number, c: number): boolean {
-        return ((a + b > c) && (a + c > b) && (b + c > a) && (a + b != c) && (a + c != b) && (b + c != a) && (a > 0) && (b > 0) && (c > 0)) ? true : false
+        return (a + b > c) && (a + c > b) && (b + c > a)
     };
     console.log(getThreeSidesOfTriangle(2, 2, 3));
     console.log(getThreeSidesOfTriangle(-1, 2, 3));
@@ -138,14 +147,12 @@ console.log(`TASK_1.ADVANCED LEVEL`);
 Если входные данные недействительны, вы должны вернуть 0 (поскольку надломы не требуются, если у нас нет шоколада для разделения). Ввод всегда будет неотрицательным целым числом.
 */
 console.log(`TASK_2.ADVANCED LEVEL`);
-
 function getAmountOfChocolateSpread(a: number, b: number, side: Function): void {
     let minNumOfSpreads = side(a, b);
     minNumOfSpreads > 0 ? console.log(`Минимальное количество разломов: ${(minNumOfSpreads)}`) :
-        console.log(`Входные данные недействительны: 0`)
+        console.log(`Входные данные недействительны: 0`);
 };
 getAmountOfChocolateSpread(1, 1, side);
-
 function side(a: number, b: number): number {
     return ((a * b) - 1);
 };
@@ -160,60 +167,34 @@ function side(a: number, b: number): number {
 */
 console.log('`TASK_3.ADVANCED LEVEL`');
 {
-    function buyingPhoneAndStaf(myAccount: number, tax: number, minPriceOfPhone: number, maxPriceOfPhone: number, minPriceOfStaff: number, maxPriceOfStaff: number,
-        getBalanceOfAccount: Function, getTax: Function, getPriceOfPhone: Function, getPriceOfStaff: Function) {
-
-        let myBalance = getBalanceOfAccount(myAccount);
-        let mytax = getTax(tax);
-        let phonePrice = getPriceOfPhone(minPriceOfPhone, maxPriceOfPhone);
-        let staffPrice = getPriceOfStaff(minPriceOfStaff, maxPriceOfStaff);
+    // -------------------------------------------------------------------[ВЫЗОВ ФУНКЦИИ]------------------------------------------
+    getFinallyFormattedPrice(10000, 1000, 2000, 100, 200, 20, getPriceOfPhoneAndStaff, getCalcTax, buyingPhoneAndStaff);
+    // -----------------------Функция вывода окончательной стоимости купленного товара с валютой и округлением до 2-х знаков после запятой.------------------------------------------
+    function getFinallyFormattedPrice(myAccount: number, minPriceOfPhone: number, maxPriceOfPhone: number, minPriceOfStaff: number, maxPriceOfStaff: number, tax: number,
+        getPriceOfPhoneAndStaff: Function, getCalcTax: Function, buyingPhoneAndStaff: Function) {
+        let finPrice = buyingPhoneAndStaff(myAccount, minPriceOfPhone, maxPriceOfPhone, minPriceOfStaff, maxPriceOfStaff, tax, getPriceOfPhoneAndStaff, getCalcTax);
+        return (finPrice < myAccount) ? console.log(`Окончательная цена покупки: ${finPrice.toFixed(2)} BYN`) : (`У вас недостаточно редств`);
+    };
+    // -----------------------Функция процесса покупки пока не закончатся деньги------------------------------------------
+    function buyingPhoneAndStaff(myAccount: number, minPriceOfPhone: number, maxPriceOfPhone: number, minPriceOfStaff: number, maxPriceOfStaff: number, tax: number,
+        getPriceOfPhoneAndStaff: Function, getCalcTax: Function, getFinallyFormattedPrice: Function) {
         let totalSumReceipt = 0;
-
-        for (let i = phonePrice + staffPrice; i <= myBalance; i++) {
-            totalSumReceipt = i++
-            
-        }
-        let totalSumReceiptWithTax: string = (totalSumReceipt + ((totalSumReceipt / 100) * mytax)).toFixed(2);
-        console.log(`Окончательная сумма покупки: ${totalSumReceiptWithTax} BYN`);
-        console.log(`Ваш баланс: ${myBalance} BYN`);
-        return +totalSumReceiptWithTax <= myBalance ? console.log(`Вы купили телефонов и аксессуаров на сумму: ${totalSumReceiptWithTax} BYN`) : console.log(`У Вас недостаточно средств`);
-    }
-    buyingPhoneAndStaf(10000, 20, 2000, 3000, 253, 299, getBalanceOfAccount, getTax, getPriceOfPhone, getPriceOfStaff);
-
-
-
-
-
-    function getPriceOfPhone(minPriceOfphone: number, maxPriceOfPhone: number): number {
-        let random1 = (Math.random() * (maxPriceOfPhone - minPriceOfphone) + minPriceOfphone).toFixed(2);
+        let calcSumOfPriceAndStaffWithtax = getCalcTax(minPriceOfPhone, maxPriceOfPhone, minPriceOfStaff, maxPriceOfStaff, tax, getPriceOfPhoneAndStaff);
+        for (let i = calcSumOfPriceAndStaffWithtax; i <= myAccount; i++) {
+            totalSumReceipt = i++;
+        };
+        return +totalSumReceipt;
+    };
+    // -----------------------Функция расчёта случайной цены телефона и аксессуара.------------------------------------------
+    function getPriceOfPhoneAndStaff(minPriceOfphoneAndStaff: number, maxPriceOfPhoneAndStaff: number): number {
+        let random1 = (Math.random() * (maxPriceOfPhoneAndStaff - minPriceOfphoneAndStaff) + minPriceOfphoneAndStaff).toFixed(2);
         return +random1;
-    }
-
-    function getPriceOfStaff(minStaffOfphone: number, maxStaffOfPhone: number): number {
-        let random2 = (Math.random() * (maxStaffOfPhone - minStaffOfphone) + minStaffOfphone).toFixed(2);
-        return +random2;
-    }
-
-    function getBalanceOfAccount(balance: number) {
-        return balance;
-    }
-
-    function getTax(tax: number) {
-        return tax;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    };
+    // -----------------------Функция расчёта налога и добавление его к цене.------------------------------------------------
+    function getCalcTax(minPriceOfPhone: number, maxPriceOfPhone: number, minPriceOfStaff: number, maxPriceOfStaff: number, cTax: number, getPriceOfPhoneAndStaff: Function) {
+        let phonePrice = getPriceOfPhoneAndStaff(minPriceOfPhone, maxPriceOfPhone);
+        let staffPrice = getPriceOfPhoneAndStaff(minPriceOfStaff, maxPriceOfStaff);
+        let sumOfPhonesAndstaff = phonePrice + staffPrice;
+        return (((sumOfPhonesAndstaff) + ((sumOfPhonesAndstaff / 100) * cTax))).toFixed(2);
+    };
 }
