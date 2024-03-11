8f5357a77a0b0d2a7ba6e89193f7b3bd81b98c60
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-11 17:02:21 +0300

    fast commit

--- H-W-7-PART-1/H-W-7-PART-1.ts
+++ H-W-7-PART-1/H-W-7-PART-1.ts
@@ -5,7 +5,7 @@ const colors = ['red', 'green', 'blue']
 */
 console.log(`TASK 1:`);
 {
-    let colors = ['red', 'green', 'blue'];
+    let colors: string[] = ['red', 'green', 'blue'];
     console.log(`Длинна нашего массива = ${colors.length}`);
 }
 /* [TASK 2] Дан массив:
@@ -14,8 +14,10 @@ const animals = ['monkey', 'dog', 'cat']
 */
 console.log(`TASK 2:`);
 {
-    let animals = ['monkey', 'dog', 'cat'];
+
+    let animals: string[] = ['monkey', 'dog', 'cat'];
     console.log(animals[animals.length - 1]);
+    console.log(_.last(animals));
 }
 /* [TASK 3] Дан массив:
     const numbers = [5, 43, 63, 23, 90]
@@ -24,23 +26,17 @@ console.log(`TASK 2:`);
 */
 console.log(`TASK 3:`);
 {
-    let numbers1 = [5, 43, 63, 23, 90];
+    let numbers1: number[] = [5, 43, 63, 23, 90];
     for (let i: number = 0; i < numbers1.length; i++) {
         numbers1.splice(i);
     }
-    console.log(numbers1);
+    console.log(numbers1.length);
 
-    let numbers2 = [5, 43, 63, 23, 90];
+    let numbers2: number[] = [5, 43, 63, 23, 90];
     for (let i: number = 0; i < numbers2.length; i++) {
         numbers2.slice(i);
     }
-    console.log(numbers1);
-
-    let numbers3 = [5, 43, 63, 23, 90];
-    for (let i: number = 0; i < numbers3.length; i++) {
-        delete numbers3[i];
-    }
-    console.log(numbers3);
+    console.log(numbers1.length);
 }
 /* [TASK 4] Дан массив:
     const students = ['Polina', 'Dasha', 'Masha']
@@ -50,7 +46,7 @@ console.log(`TASK 3:`);
 */
 console.log(`TASK 4:`);
 {
-    let students = ['Polina', 'Dasha', 'Masha'];
+    let students: string[] = ['Polina', 'Dasha', 'Masha'];
     let delLastElemAndAdd = students.splice(students.length - 1, students.length - 1, `Borya`);
     console.log(students);
     let delFirstElemAndAdd = students.splice(1, 1, `Andrey`);
@@ -62,12 +58,12 @@ console.log(`TASK 4:`);
 */
 console.log(`TASK 5:`);
 {
-    let cats1 = ['Gachito', 'Tom', 'Batman'];
+    let cats1: string[] = ['Gachito', 'Tom', 'Batman'];
     for (let i: number = 0; i < cats1.length; i++) {
         console.log(cats1[i]);
     }
     console.log(`----------`)
-    let cats2 = ['Gachito', 'Tom', 'Batman'];
+    let cats2: string[] = ['Gachito', 'Tom', 'Batman'];
     for (let value of cats2) {
         console.log(value);
     }
@@ -76,24 +72,83 @@ console.log(`TASK 5:`);
     const evenNumbers = [2, 4, 6, 8, 10]
     const oddNumbers = [1, 3, 5, 7, 9]
 + Соедините два массива чисел в один.
++ В полученном массиве попробуйте найти индекс числа **`8`**
 */
 console.log(`TASK 6:`);
 {
-    let evenNumbers = [2, 4, 6, 8, 10];
-    let oddNumbers = [1, 3, 5, 7, 9];
-    let allNumbers = evenNumbers.concat(oddNumbers);
-    console.log(allNumbers);
+    let evenNumbers: number[] = [2, 4, 6, 8, 10];
+    let oddNumbers: number[] = [1, 3, 5, 7, 9];
+    let allNumbers: number[] = evenNumbers.concat(oddNumbers);
+    console.log(`Индекс числа 8 = ${allNumbers.indexOf(8)}`);
 }
 /* [TASK 7] 
 const binary = [0, 0, 0, 0]
 + Наш бинарный массив неполный, в нем явно не хватает единиц.
 + Превратите данный массив в строку.
 
+
 > [0, 0, 0, 0] -> '0101010'
 */
 console.log(`TASK 7:`)
 {
-    let binary = [0, 0, 0, 0];
-    let fullBunary = binary.map(getFullBunary => getFullBunary === 0 ? `01` : `1`).join('');
+    let binary: number[] = [0, 0, 0, 0];
+    let fullBunary: string = binary.map(getFullBunary => getFullBunary === 0 ? `01` : `1`).join('');
     console.log(fullBunary);
 }
+/* [TASK 1. Advanced level] 
+Реализуйте функцию которая будет проверять, является ли слово палиндромом.
+*/
+console.log(`TASK 1. Advanced level`);
+{
+    function getStringtoArray(str: string): Array<string> {
+        let arr: string[] = str.split(``);
+        let polind: string[] = str.split(``).reverse();
+        console.log(arr.toString() == polind.toString() ? `Your word is polindrom` : `Your word is not polindrom`);
+        return polind;
+    }
+    getStringtoArray(`tenet`);
+}
+/* [TASK 2. Advanced level] 
+    const matrix = [
+        [12, 98, 78, 65, 23],
+        [54, 76, 98, 43, 65],
+        [13, 324, 65, 312],
+        [9092, 22, 45, 90000],
+    ]
+> Выведите в консоль среднее значение чисел в многомерном массиве.
+*/
+console.log(`TASK 2. Advanced level`);
+{
+    let matrix = [
+        [12, 98, 78, 65, 23],
+        [54, 76, 98, 43, 65],
+        [13, 324, 65, 312],
+        [9092, 22, 45, 90000],
+    ];
+    let totalSum: number = 0;
+    let totalLength: number = 0;
+    let totalLength2: number = 0;
+    matrix.forEach((line) => {
+        totalLength += line.length;
+        line.forEach((value) => {
+            totalLength2++;
+            totalSum += value;
+        });
+    });
+    console.log(`Avg value = ${totalSum / totalLength2}`);
+
+    let flatteredArray = _.flattenDeep(matrix);
+    let totalSum_v3: number = flatteredArray.reduce((accumulator, currentValue) => {
+        accumulator += currentValue;
+        return accumulator;
+    }, 0);
+    
+    let totalLength_v3: number = flatteredArray.length;
+    console.log(`V3 - avg value = ${totalSum_v3 / totalLength_v3}`);
+}
+/* [TASK 3. Advanced level] 
+   const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4]
+
+Создайте два массива, в один поместите все положительные числа включая 0, в другой все отрицательные. Оба массива затем выведите в консоль. 
+*/
+console.log(`TASK 3. Advanced level`);
