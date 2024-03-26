bf8e0872796dcf7edb1df3339b8a967425af7368
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-26 21:45:00 +0300

    add: 3rd task. Ready for review

--- H-W-10/HW10.ts
+++ H-W-10/HW10.ts
@@ -6,37 +6,38 @@
 Выводящий текст должен быть таким: `This is <carName>. It has <engineType> engine and max speed equal to <maxSpeed>. Approximetly cost of the car is <carCost>`. 
 (В задании используйте не только public модификатор, где это возможно)
 */
+console.log(`Task 1:`);
 {
     class Car {
-        constructor(protected typeOfCar: string, protected typeOfEngine: string) {}
-    
+        constructor(protected typeOfCar: string, protected typeOfEngine: string) { };
+
         public getCarInfo(): string {
             return `This is ${this.typeOfCar}. It has ${this.typeOfEngine} engine`;
-        }
-    }
-    
+        };
+    };
+
     class SportCar extends Car {
         constructor(typeOfCar: string, typeOfEngine: string, private maxSpeed: number, private price: number) {
             super(typeOfCar, typeOfEngine);
-        }
-    
+        };
+
         public getCarInfo(): string {
             return `${super.getCarInfo()} and max speed equal to ${this.maxSpeed}. Approximate cost of the car is ${this.price}.`;
-        }
-    }
-    
+        };
+    };
+
     class LuxuryCar extends Car {
         constructor(typeOfCar: string, typeOfEngine: string, private maxSpeed: number, private price: number) {
             super(typeOfCar, typeOfEngine);
-        }
-    
+        };
+
         public getCarInfo(): string {
             return `${super.getCarInfo()} and max speed equal to ${this.maxSpeed}. Approximate cost of the car is ${this.price}.`;
-        }
-    }
+        };
+    };
     let luxuryCar1 = new LuxuryCar('Mercedes-Benz', 'ICE', 300, 2000000);
     console.log(luxuryCar1.getCarInfo());
-    
+
     let sportCar1 = new SportCar('BMW', 'Electric', 220, 1000000);
     console.log(sportCar1.getCarInfo());
 }
@@ -45,16 +46,17 @@
 Создайте два объекта Person. Реализуйте метод, который выводит строку `My name is <this.name>`. 
 И в зависимости от контекста выполнения, этот метод должен выводить соответвующее имя
 */
+console.log(`Task 2:`);
 {
-    let freePreson : {name: string} = {
+    let freePreson: { name: string } = {
         name: 'Ilya1'
     };
 
-    let busyPreson : {name: string} = {
+    let busyPreson: { name: string } = {
         name: 'Ilya2'
     };
 
-    function getMyNameIs () {
+    function getMyNameIs() {
         return `My name is ${this.name}`
     };
 
@@ -68,3 +70,20 @@
 В зависимости от контекста выполнения, этот метод должен выводить соответвующую информацию о машинах. 
 К примеру `This car has 3 doors and this is left-hand drive car`
 */
+console.log(`Task 3:`);
+{
+    let firstCar: { myCar: string } = {
+        myCar: "Maybach"
+    };
+
+    let secondCar: { myCar: string } = {
+        myCar: "Toyota"
+    };
+
+    function Car(amountOfDoors: number, steeringPosition: string) {
+        return `${this.myCar} has ${amountOfDoors} doors and this is ${steeringPosition}-hand drive car`
+    };
+
+    console.log(Car.call(firstCar, 4, 'left'));
+    console.log(Car.call(secondCar, 3, 'right'));
+}
