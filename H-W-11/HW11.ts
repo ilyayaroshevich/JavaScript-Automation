ad20ab58fe0633f6e0e1190152fe358d25eaec34
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-28 18:50:06 +0300

    modify: Task 1.HW11. Rename variables and functions, remove unnecessary brackets

--- H-W-11/HW11.ts
+++ H-W-11/HW11.ts
@@ -11,25 +11,25 @@ import fetch from 'cross-fetch';
 - Async / await
 */
 {
-    function getID(postId: number): any {                                         // Создание функции, которая принимает нужный номер поста
-        return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)        // Возвращает Promise
-            .then((response) => {                                                   // Работа с вовзращённым Promise
-                if (!response.ok) {                                                 // Используется метод встроенного класса "response".                                                                               
-                    throw new Error(`${response.status} (${response.statusText})`); // Если статус код response не 2XX, то пробрасываем кастомную ошибку
+    function getPasedJson(postId: number): any {                                            // Создание функции, которая принимает нужный номер поста
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)         // Возвращает Promise
+            .then(response => {                                                      // Работа с вовзращённым Promise
+                if (!response.ok) {                                                  // Используется метод встроенного класса "response".                                                                               
+                    throw new Error(`${response.status} (${response.statusText})`);  // Если статус код response не 2XX, то пробрасываем кастомную ошибку
                 }
-                return response.json();                                             // В ином случае - парсим ответ в JSON
+                return response.json();                                              // В ином случае - парсим ответ в JSON
             });
     }
-    const postsId: number[] = [3, 7, 15, 23];                                       // Создаём массив из ID, которые нам нужно достать из списка постов
-    const arrToFetch = postsId.map((post) => getID(post));                          // Создаём массив Промисов
+    const IdPosts: number[] = [3, 7, 15, 23];                                        // Создаём массив из ID, которые нам нужно достать из списка постов
+    const promiseArray = IdPosts.map((post) => getPasedJson(post));                    // Создаём массив Промисов
 
-    Promise.all(arrToFetch)                                                         // Используется метод Promise.all с аргументом в ввиде массива промисов, что бы в случае невыполнения хотя бы одного из них - появилась ошибка.
-        .then((specificPosts) => {                                                  // С помощью .then и функции arrToFetch работаем с полученным массивом из промисов
-            specificPosts.forEach((post) => {                                       // С помощью "forEach" достаём каждый элемент массива и выводим его в консоль
-                console.log(post);                                                  // Вывод каждого элемента массива в консоль
+    Promise.all(promiseArray)                                                          // Используется метод Promise.all с аргументом в ввиде массива промисов, что бы в случае невыполнения хотя бы одного из них - появилась ошибка.
+        .then((iteratingArray) => {                                                     // С помощью .then и функции arrToFetch работаем с полученным массивом из промисов
+            iteratingArray.forEach(post => {                                          // С помощью "forEach" достаём каждый элемент массива и выводим его в консоль
+                console.log(post);                                                   // Вывод каждого элемента массива в консоль
             });
         })
-        .catch((error) => {                                                         // .catch помогает обрабатывать ошибки
+        .catch(error => {                                                            // .catch помогает обрабатывать ошибки
             console.error(`Error fetching data:\n`, error);                         // используем метод "console.error" что бы локировать ошибки в консоль
         });
 }
@@ -37,27 +37,27 @@ import fetch from 'cross-fetch';
 
 
 {
-    async function getID(postsIds: number): Promise<object> {                                   // Используем async функцию, что бы была возможность использовать await вместо .then                     
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postsIds}`)  // С помощью await мы ждём, пока fetch выполнится и только тогда мы идём дальше
+    async function getPasedJson(postId: number): Promise<object> {                                   // Используем async функцию, что бы была возможность использовать await вместо .then                     
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)  // С помощью await мы ждём, пока fetch выполнится и только тогда мы идём дальше
         if (!response.ok) {                                                                     // Обрабатываем ошибку в лсучае, если статус код ответа будет !== 2ХХ
             throw new Error(`${response.status} (${response.statusText})`);                     // кастомная ошибка
-          }
+        }
         return await response.json();                                                           // С помощью await жёдм пока тело ответа парсится в JSON
     }
-    const postsId: number[] = [3, 7, 15, 23];                                                   // Массив IDs, которые нам нужно достать из сервера
+    const IdPosts: number[] = [3, 7, 15, 23];                                                   // Массив IDs, которые нам нужно достать из сервера
 
     async function fetchPosts() {                                                               // Используем async функцию, что бы была возможность использовать await вместо .then
-        const arrToFetch = postsId.map(async (post) => await getID(post));                      // С помощью метода .map перебираем изначальный массив и создаём новый массив, состоящий из промисов.
-        const specificPosts = await Promise.all(arrToFetch);                                    // С помощью await ждём пока все промисы зарезолвятся
-        specificPosts.forEach((post: object) => {                                               // С помощью ForEach перебираем все элементы полученного массива и выводим их в консоль
-          console.log(post);                                                                    // Вывод элементов массива в консоль
+        const promiseArray = IdPosts.map(async (post) => await getPasedJson(post));                      // С помощью метода .map перебираем изначальный массив и создаём новый массив, состоящий из промисов.
+        const iteratedArray = await Promise.all(promiseArray);                                    // С помощью await ждём пока все промисы зарезолвятся
+        iteratedArray.forEach((post: object) => {                                               // С помощью ForEach перебираем все элементы полученного массива и выводим их в консоль
+            console.log(post);                                                                  // Вывод элементов массива в консоль
         });
-      }
-      
-      fetchPosts().catch((error) => {                                                           // Обрабатываем ошибки, полученные в результате резолва промисов
+    }
+
+    fetchPosts().catch(error => {                                                               // Обрабатываем ошибки, полученные в результате резолва промисов
         console.error(`Error fetching data:\n`, error);                                         // Вывод ошибок в консоль
-      });                            
-    
+    });
+
 }
 
 
