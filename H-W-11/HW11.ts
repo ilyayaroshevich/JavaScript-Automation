9085e743520ca84facfb8cda47045dbe5f0e5a7a
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-03-29 16:03:21 +0300

    add: types, interface. modify: catchinh errors

--- H-W-11/HW11.ts
+++ H-W-11/HW11.ts
@@ -10,51 +10,58 @@ import fetch from 'cross-fetch';
 - Promise chaining
 - Async / await
 */
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+type FetchPostFunction = (postId: number) => Promise<Post>;
 {
-    function getPasedJson(postId: number): any {                                        
-        return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)            
-            .then(response => {                                                         
-                if (!response.ok) {                                                                                                                                    
-                    throw new Error(`${response.status} (${response.statusText})`);     
+    const getPasedJson: FetchPostFunction = (postId: number) => {
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status} (${response.statusText})`);
                 };
-                return response.json();                                                 
+                return response.json();
             });
     }
-    const IdPosts: number[] = [3, 7, 15, 23];                                           
-    const promiseArray = IdPosts.map((post) => getPasedJson(post));                     
+    const IdPosts: number[] = [3, 7, 15, 23];
+    const promiseArray: Promise<Post>[] = IdPosts.map((post) => getPasedJson(post));
 
-    Promise.all(promiseArray)                                                           
-        .then((iteratingArray) => {                                                     
-            iteratingArray.forEach(post => {                                            
-                console.log(post);                                                      
+    Promise.all(promiseArray)
+        .then((iteratingArray) => {
+            iteratingArray.forEach(post => {
+                console.log(post);
             });
         })
-        .catch(error => {                                                               
-            console.error(`Error fetching data:\n`, error);                             
+        .catch(error => {
+            console.error(`Error fetching data:\n`, error);
         });
 }
 {
-    const getPasedJson = async (postId: number): Promise<object> => {                                                       
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)        
-        if (!response.ok) {                                                                         
-            throw new Error(`${response.status} (${response.statusText})`);                         
+    const getPasedJson: FetchPostFunction = async (postId: number) => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+        if (!response.ok) {
+            throw new Error(`${response.status} (${response.statusText})`);
         };
         const jsonResponse = await response.json();
-        return jsonResponse;                                                               
+        return jsonResponse;
     };
 
-    const IdPosts: number[] = [3, 7, 15, 23];                                                       
+    const IdPosts: number[] = [3, 7, 15, 23];
 
-    const fetchPosts = async () => {                                                                   
-        const promiseArray = IdPosts.map(async (post) => await getPasedJson(post));                 
-        const iteratedArray = await Promise.all(promiseArray);                                      
-        iteratedArray.forEach((post: object) => {                                                  
-            console.log(post);                                                                      
+    const fetchPosts = async () => {
+        const promiseArray = IdPosts.map(async (post) => await getPasedJson(post));
+        const iteratedArray: Post[] = await Promise.all(promiseArray);
+        iteratedArray.forEach((post: object) => {
+            console.log(post);
         });
     };
 
-    fetchPosts().catch(error => {                                                                   
-        console.error(`Error fetching data:\n`, error);                                             
+    fetchPosts().catch(error => {
+        console.error(`Error fetching data:\n`, error);
     });
 }
 /*
@@ -65,46 +72,55 @@ import fetch from 'cross-fetch';
     - Функция `getTodos` делает запрос по указанному адресу и забирает данные.
     - Функция `printTodos` создает массив объектов, объект записывает id и title объекта с делом. Вывести полученный результат в консоль
 */
+interface Todo {
+    id: number;
+    title: string;
+}
 {
-    function getTodos() {                                                                                       
-        return fetch(`https://jsonplaceholder.typicode.com/todos`)                  
-            .then(response => {                                                     
-                if (!response.ok) {                                                 
-                    throw new Error(`${response.status} ${response.statusText}`)    
+    const getTodos = (): Promise<Todo[]> => {
+        return fetch(`https://jsonplaceholder.typicode.com/todos`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status} ${response.statusText}`)
                 };
-                    return response.json()                                          
-            }) 
-            .catch(error => {                                                       
+                return response.json()
+            })
+            .catch(error => {
                 console.error(`Error fetching todos: ${error.message}`);
             });
     };
 
-   const printTodos = (todos) => {                                                      
-    const todoList = todos.map(todo => {                                            
-        return {id: todo.id, title: todo.title};                                    
-    });
-       console.log(todoList);                                                       
+    const printTodos = (todos: Todo[]) => {
+        const todoList: Todo[] = todos.map((todo: Todo) => {
+            return { id: todo.id, title: todo.title };
+        });
+        console.log(todoList);
     };
 
-getTodos()                                                                          
-  .then(todos => {                                                                   
-    printTodos(todos);                                                              
-  })
-  .catch(error => {                                                                 
-    console.error(error.message);                                                   
-  });
-} 
+    getTodos()
+        .then((todos: Todo[]) => {
+            printTodos(todos);
+        })
+        .catch(error => {
+            console.error(error.message);
+        });
+}
+interface Todo {
+    id: number;
+    title: string;
+}
 {
-    const getTodos= async () => {
+    const getTodos = async (): Promise<Todo[]> => {
         try {
-        const fetchTodos = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-        if(!fetchTodos.ok) {
-            throw new Error(`Response error: ${fetchTodos.status} ${fetchTodos.statusText}`);
-        };
-        const jsonTodos = await fetchTodos.json();
-        return jsonTodos;
+            const fetchTodos = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+            if (!fetchTodos.ok) {
+                throw new Error(`Response error: ${fetchTodos.status} ${fetchTodos.statusText}`);
+            };
+            const jsonTodos = await fetchTodos.json();
+            return jsonTodos;
         } catch (error) {
-            console.log(`Error fetching todos: ${error.message}`)
+            console.error(`Error fetching todos: ${error.message}`);
+            return Promise.reject(`Error fetching todos: ${error.message}`);
         };
     };
 
@@ -114,13 +130,13 @@ getTodos()
             if (!todosFunc) {
                 return;
             };
-        const  newObjects = todosFunc.map(todos => {
-            return {id: todos.id, title: todos.title}
-        });
-        console.log(newObjects)
-    } catch (error) {
-        console.log(`Error printing todos: ${error.message}`);
-    };
+            const newObjects: Todo[] = todosFunc.map(todos => {
+                return { id: todos.id, title: todos.title }
+            });
+            console.log(newObjects)
+        } catch (error) {
+            console.error(`Error printing todos: ${error}`);
+        };
     };
     printTodos();
 }
