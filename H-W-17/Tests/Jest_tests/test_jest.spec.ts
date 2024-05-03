b1b69b0528aca1254fff4430ec24105797a23106
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-03 17:45:17 +0300

    Feat: Changing files with tests;
    - Added "beforeAll" and "before" to create Calculator class before tests and to notify when tests are started;
    - Added "afterAll" and "after"  to notify when tests are completed;

--- H-W-17/Tests/Jest_tests/test_jest.spec.ts
+++ H-W-17/Tests/Jest_tests/test_jest.spec.ts
@@ -1,54 +1,64 @@
 import { Calculator } from "../../Src/CalculatorClass";
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test, beforeEach, afterEach } from '@jest/globals';
 
 describe("Jest tests", () => {
+    let calculator: Calculator;
+    beforeAll(() => {
+        calculator = new Calculator();
+        console.log(`Тестирование начато!`);
+    });
+
     test("checks additions two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(2, 3, 'add');
+        const result: number = calculator.calculate(2, 3, 'add');
         expect(result).toBe(5);
     });
+
     test("checks additions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-2, 3, 'add');
+        const result: number = calculator.calculate(-2, 3, 'add');
         expect(result).toBe(1);
     });
+
     test("checks subtractions two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(5, 2, 'subtract');
+        const result: number = calculator.calculate(5, 2, 'subtract');
         expect(result).toBe(3);
     });
+
     test("checks subtractions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-5, 2, 'subtract');
+        const result: number = calculator.calculate(-5, 2, 'subtract');
         expect(result).toBe(-7);
     });
+
     test("checks multiply two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 2, 'multiply');
+        const result: number = calculator.calculate(3, 2, 'multiply');
         expect(result).toBe(6);
     });
+
     test("checks multiply one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-3, 2, 'multiply');
+        const result: number = calculator.calculate(-3, 2, 'multiply');
         expect(result).toBe(-6);
     });
+
     it("checks multiply one positive and one 0", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 0, 'multiply');
+        const result: number = calculator.calculate(3, 0, 'multiply');
         expect(result).toBe(0);
     });
+
     test("checks dividing two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(10, 2, 'divide');
+        const result: number = calculator.calculate(10, 2, 'divide');
         expect(result).toBe(5);
     });
+
     test("checks dividing one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-10, 2, 'divide');
+        const result: number = calculator.calculate(-10, 2, 'divide');
         expect(result).toBe(-5);
     });
+
     it("checks dividing one positive and one 0", () => {
-        const calculator = new Calculator();
         expect(() => calculator.calculate(10, 0, 'divide')).toThrowError(Error);
     });
+
+    afterAll(() => {
+        console.log(`Тестирование закончено!`)
+    });
+
 });
--- H-W-17/Tests/Mocha_tests/tests_mocha.spec.ts
+++ H-W-17/Tests/Mocha_tests/tests_mocha.spec.ts
@@ -4,108 +4,106 @@ import { expect } from "chai";
 
 
 describe("Mocha + asserts tests", () => {
+    let calculator: Calculator;
+    before(() => {
+        calculator = new Calculator();
+        console.log(`Тестирование начато!`);
+    });
+
     it("checks additions two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(2, 3, 'add');
+        const result: number = calculator.calculate(2, 3, 'add');
         assert.equal(result, 5);
     });
     it("checks additions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-2, 3, 'add');
+        const result: number = calculator.calculate(-2, 3, 'add');
         assert.equal(result, 1);
     });
     it("checks subtractions two positive digits", () => {
         const calculator = new Calculator();
-        const result = calculator.calculate(5, 2, 'subtract');
+        const result: number = calculator.calculate(5, 2, 'subtract');
         assert.equal(result, 3);
     });
     it("checks subtractions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-5, 2, 'subtract');
+        const result: number = calculator.calculate(-5, 2, 'subtract');
         assert.equal(result, -7);
     });
     it("checks multiply two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 2, 'multiply');
+        const result: number = calculator.calculate(3, 2, 'multiply');
         assert.equal(result, 6);
     });
     it("checks multiply one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-3, 2, 'multiply');
+        const result: number = calculator.calculate(-3, 2, 'multiply');
         assert.equal(result, -6);
     });
     it("checks multiply one positive and one 0", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 0, 'multiply');
+        const result: number = calculator.calculate(3, 0, 'multiply');
         assert.equal(result, 0);
     });
     it("checks dividing two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(10, 2, 'divide');
+        const result: number = calculator.calculate(10, 2, 'divide');
         assert.equal(result, 5);
     });
     it("checks dividing one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-10, 2, 'divide');
+        const result: number = calculator.calculate(-10, 2, 'divide');
         assert.equal(result, -5);
     });
     it("checks dividing one positive and one 0", () => {
-        const calculator = new Calculator();
         assert.throws(() => {
             calculator.calculate(10, 0, 'divide');
         }, Error);
     });
+    after(() => {
+        console.log(`Тестирование закончено!`)
+    });
 });
 
 describe("Mocha + chai tests", () => {
+    let calculator: Calculator;
+    before(() => {
+        calculator = new Calculator();
+        console.log(`Тестирование начато!`);
+    });
     it("checks additions two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(2, 3, 'add');
+        const result: number = calculator.calculate(2, 3, 'add');
         expect(result).to.equal(5, "Result is not equal to expected result");
     });
     it("checks additions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-2, 3, 'add');
+        const result: number = calculator.calculate(-2, 3, 'add');
         expect(result, "Result is not equal to expected result").to.equal(1);
     });
     it("checks subtractions two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(5, 2, 'subtract');
+        const result: number = calculator.calculate(5, 2, 'subtract');
         expect(result, "Result is not equal to expected result").to.equal(3);
     });
     it("checks subtractions one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-5, 2, 'subtract');
+        const result: number = calculator.calculate(-5, 2, 'subtract');
         expect(result, "Result is not equal to expected result").to.equal(-7);
     });
     it("checks multiply two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 2, 'multiply');
+        const result: number = calculator.calculate(3, 2, 'multiply');
         expect(result, "Result is not equal to expected result").to.equal(6);
     });
     it("checks multiply one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-3, 2, 'multiply');
+        const result: number = calculator.calculate(-3, 2, 'multiply');
         expect(result, "Result is not equal to expected result").to.equal(-6);
     });
     it("checks multiply one positive and one 0", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(3, 0, 'multiply');
+        const result: number = calculator.calculate(3, 0, 'multiply');
         expect(result, "Result is not equal to expected result").to.equal(0);
     });
     it("checks dividing two positive digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(10, 2, 'divide');
+        const result: number = calculator.calculate(10, 2, 'divide');
         expect(result, "Result is not equal to expected result").to.equal(5);
     });
     it("checks dividing one positive and one negative digits", () => {
-        const calculator = new Calculator();
-        const result = calculator.calculate(-10, 2, 'divide');
+        const result: number = calculator.calculate(-10, 2, 'divide');
         expect(result, "Result is not equal to expected result").to.equal(-5);
     });
     it("checks dividing one positive and one 0", () => {
-        const calculator = new Calculator();
         expect(calculator.calculate.bind(calculator, 10, 0, 'divide')).to.throw(Error);
+    });
+    after(() => {
+        console.log(`Тестирование закончено!`)
     });
 });
 
