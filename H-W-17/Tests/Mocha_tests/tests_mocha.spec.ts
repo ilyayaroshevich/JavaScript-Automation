b5fa13b487e2d4ffc3dadbfe87b693c0750b08a9
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-03 09:25:30 +0300

    Fix: Fix the "Mocha + chai" unit-tests
    - The lastes "chai" version was removed;
    - chai@4.4.1 was installed.

--- H-W-17/Tests/Mocha_tests/tests_mocha.spec.ts
+++ H-W-17/Tests/Mocha_tests/tests_mocha.spec.ts
@@ -3,59 +3,59 @@ import * as assert from 'assert';
 import { expect } from "chai";
 
 
-// describe("Mocha + asserts tests", () => {
-//     it("checks additions two positive digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(2, 3, 'add');
-//         assert.equal(result, 5);
-//     });
-//     it("checks additions one positive and one negative digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(-2, 3, 'add');
-//         assert.equal(result, 1);
-//     });
-//     it("checks subtractions two positive digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(5, 2, 'subtract');
-//         assert.equal(result, 3);
-//     });
-//     it("checks subtractions one positive and one negative digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(-5, 2, 'subtract');
-//         assert.equal(result, -7);
-//     });
-//     it("checks multiply two positive digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(3, 2, 'multiply');
-//         assert.equal(result, 6);
-//     });
-//     it("checks multiply one positive and one negative digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(-3, 2, 'multiply');
-//         assert.equal(result, -6);
-//     });
-//     it("checks multiply one positive and one 0", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(3, 0, 'multiply');
-//         assert.equal(result, 0);
-//     });
-//     it("checks dividing two positive digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(10, 2, 'divide');
-//         assert.equal(result, 5);
-//     });
-//     it("checks dividing one positive and one negative digits", () => {
-//         const calculator = new Calculator();
-//         const result = calculator.calculate(-10, 2, 'divide');
-//         assert.equal(result, -5);
-//     });
-//     it("checks dividing one positive and one 0", () => {
-//         const calculator = new Calculator();
-//         assert.throws(() => {
-//             calculator.calculate(10, 0, 'divide');
-//         }, Error);
-//     });
-// });
+describe("Mocha + asserts tests", () => {
+    it("checks additions two positive digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(2, 3, 'add');
+        assert.equal(result, 5);
+    });
+    it("checks additions one positive and one negative digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(-2, 3, 'add');
+        assert.equal(result, 1);
+    });
+    it("checks subtractions two positive digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(5, 2, 'subtract');
+        assert.equal(result, 3);
+    });
+    it("checks subtractions one positive and one negative digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(-5, 2, 'subtract');
+        assert.equal(result, -7);
+    });
+    it("checks multiply two positive digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(3, 2, 'multiply');
+        assert.equal(result, 6);
+    });
+    it("checks multiply one positive and one negative digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(-3, 2, 'multiply');
+        assert.equal(result, -6);
+    });
+    it("checks multiply one positive and one 0", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(3, 0, 'multiply');
+        assert.equal(result, 0);
+    });
+    it("checks dividing two positive digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(10, 2, 'divide');
+        assert.equal(result, 5);
+    });
+    it("checks dividing one positive and one negative digits", () => {
+        const calculator = new Calculator();
+        const result = calculator.calculate(-10, 2, 'divide');
+        assert.equal(result, -5);
+    });
+    it("checks dividing one positive and one 0", () => {
+        const calculator = new Calculator();
+        assert.throws(() => {
+            calculator.calculate(10, 0, 'divide');
+        }, Error);
+    });
+});
 
 describe("Mocha + chai tests", () => {
     it("checks additions two positive digits", () => {
@@ -105,8 +105,7 @@ describe("Mocha + chai tests", () => {
     });
     it("checks dividing one positive and one 0", () => {
         const calculator = new Calculator();
-        const result = calculator.calculate(-10, 2, 'divide');
-        expect(result, "Result is not equal to expected result").to.throw(Error);
+        expect(calculator.calculate.bind(calculator, 10, 0, 'divide')).to.throw(Error);
     });
 });
 
