7f3cb5401e5e57e1597c6d83d1629ab75f8c50c3
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-05 14:33:43 +0300

    Feat: ApiTests for https://reqres.in/.
    - Folder and file with const objects for testing;
    - Src folder and functions file with GET, POST, PATCH, PUT, DELETE
      requests;
    - jest.config.hs file;
    - apitests.spec.ts file with api tests;
      Test on 13-16 line does not work.

---
+++ H-W-18/Consts/consts.ts
@@ -0,0 +1,26 @@
+export let expectedDataObject: 
+{ id: number, email: string, first_name: string, last_name: string, avatar: string } = 
+{
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+}
+export let expectedSupportObject: { url: string, text: string } =
+{
+    "url": "https://reqres.in/#support-heading",
+    "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+};
+export let expectedPostObject: { name?: string | number, job?: string | number } = {
+    name: "Ilya",
+    job: "QA"
+};
+export let newData: { name: string, job: string } = {
+    name: "New Ilya",
+    job: "New QA"
+};
+export let expectedPostObjectNumbers: { name?: string | number, job?: string | number } = {
+    name: 1,
+    job: 1
+};
---
+++ H-W-18/apitests.spec.ts
@@ -0,0 +1,85 @@
+describe('GetApiTests', () => {
+    test('getListUsers', async () => {
+        let response = await getRequest('https://reqres.in/api/users?page=2');
+        expect(response.status).toBe(200);
+    });
+    test('getSingleUser', async () => {
+        let response = await getRequest(`https://reqres.in/api/users/2`);
+        expect(response.status).toBe(200);
+        expect(response.body.data).toEqual(expectedDataObject);
+        expect(response.body.support).toEqual(expectedSupportObject);
+    });
+    //                                             !I dont know how to resolve it!
+    // test('getNonExistedUser', async () => {
+    //     let response: any = await getRequest('https://reqres.in/api/users/23');
+    //     expect(response.status).toBe(404);
+    // });
+});
+
+describe('PostApiTests', () => {
+    test('createUser', async () => {
+        let response = await postRequest(`https://reqres.in/api/users`, expectedPostObject);
+        expect(response.status).toBe(201);
+        expect(response.body.name).toEqual(expectedPostObject.name);
+        expect(response.body.job).toEqual(expectedPostObject.job);
+    });
+    test('createUserWithoutName', async () => {
+        let response = await postRequest(`https://reqres.in/api/users`, expectedPostObject);
+        expect(response.status).toBe(201);
+        expect(response.body.job).toEqual(expectedPostObject.job);
+    });
+    test('createUserWithoutJob', async () => {
+        let response = await postRequest(`https://reqres.in/api/users`, expectedPostObject);
+        expect(response.status).toBe(201);
+        expect(response.body.name).toEqual(expectedPostObject.name);
+    });
+    test('createUserWithEmptyObject', async () => {
+        let response = await postRequest(`https://reqres.in/api/users`, expectedPostObject);
+        expect(response.status).toBe(201);
+        expect(typeof response.body.createdAt).toBe('string');
+        expect(typeof response.body.id).toBe('string');
+    });
+    test('createUserWithTypeofNumber', async () => {
+        let response = await postRequest(`https://reqres.in/api/users`, expectedPostObjectNumbers);
+        expect(response.status).toBe(201);
+        expect(typeof response.body.name).toBe('number');
+        expect(typeof response.body.job).toBe('number');
+    });
+});
+
+describe('PutApitests', () => {
+    test('puting', async () => {
+        let response = await putRequest(`https://reqres.in/api/users/2`, newData);
+        expect(response.status).toBe(200);
+        expect(response.body.name).toEqual(newData.name);
+        expect(response.body.job).toEqual(newData.job);
+    });
+});
+
+describe('PatchApitests', () => {
+    test('Patching', async () => {
+        let response = await patchRequest(`https://reqres.in/api/users/3`, newData);
+        expect(response.status).toBe(200);
+        expect(response.body.name).toEqual(newData.name);
+        expect(response.body.job).toEqual(newData.job);
+    });
+});
+
+describe('DeleteApitests', () => {
+    test('Deleting', async () => {
+        let response = await deleteRequest(`https://reqres.in/api/users/4`);
+        expect(response.status).toBe(204);
+    });
+});
+
+
+
+import { describe, expect, test } from '@jest/globals';
+import { getRequest, postRequest, putRequest, patchRequest, deleteRequest } from './Src/functions';
+import {
+    expectedDataObject,
+    expectedSupportObject,
+    expectedPostObject,
+    newData,
+    expectedPostObjectNumbers
+} from './Consts/consts';
