ccf856128ddb33ebe918a94e8afa12c04ddabd29
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-13 09:39:12 +0300

    update test: add new methods in Jest tests

--- H-W-17/Tests/Jest_tests/test_jest.spec.ts
+++ H-W-17/Tests/Jest_tests/test_jest.spec.ts
@@ -6,61 +6,83 @@ describe("Jest tests", () => {
     let calculator: Calculator;
     beforeAll(() => {
         calculator = new Calculator();
-        console.log(`Тестирование начато!`);
+        console.log(`Testing has started!`);
     });
 
     test("checks additions two positive digits", () => {
         const result: number = calculator.calculate(2, 3, 'add');
-        expect(result).toBe(5);
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
         expect(result).toBeGreaterThan(0);
+        expect(result).toBe(5);
+
     });
 
     test("checks additions one positive and one negative digits", () => {
         const result: number = calculator.calculate(-2, 3, 'add');
-        expect(result).toBe(1);
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
         expect(result).toBeGreaterThan(0);
+        expect(result).toBe(1);
+
     });
 
     test("checks subtractions two positive digits", () => {
         const result: number = calculator.calculate(5, 2, 'subtract');
-        expect(result).toBe(3);
         expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+        expect(result).toBeGreaterThan(0);
+        expect(result).toBe(3);
+
     });
 
     test("checks subtractions one positive and one negative digits", () => {
         const result: number = calculator.calculate(-5, 2, 'subtract');
-        expect(result).toBe(-7);
+        expect(result).toBeDefined();
         expect(result).not.toBeNull();
+        expect(result).toBeLessThan(0);
+        expect(result).toBe(-7);
+
     });
 
     test("checks multiply two positive digits", () => {
         const result: number = calculator.calculate(3, 2, 'multiply');
-        expect(result).toEqual(6);
         expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+        expect(result).toBeGreaterThan(0);
+        expect(result).toEqual(6);
+
     });
 
     test("checks multiply one positive and one negative digits", () => {
         const result: number = calculator.calculate(-3, 2, 'multiply');
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+        expect(result).toBeLessThan(0);
         expect(result).toBe(-6);
-        expect(result).not.toBeUndefined();
     });
 
     it("checks multiply one positive and one 0", () => {
         const result: number = calculator.calculate(3, 0, 'multiply');
-        expect(result).toEqual(0);
         expect(result).toBeFalsy();
+        expect(result).toBeDefined();
+        expect(result).toEqual(0);
     });
 
     test("checks dividing two positive digits", () => {
         const result: number = calculator.calculate(10, 2, 'divide');
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+        expect(result).toBeGreaterThan(0);
         expect(result).toBe(5);
-        expect(result).toBeLessThan(10);
     });
 
     test("checks dividing one positive and one negative digits", () => {
         const result: number = calculator.calculate(-10, 2, 'divide');
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+        expect(result).toBeLessThan(0);
         expect(result).toBe(-5);
-        expect(result).toBeGreaterThan(-10);
     });
 
     it("checks dividing one positive and one 0", () => {
@@ -68,7 +90,7 @@ describe("Jest tests", () => {
     });
 
     afterAll(() => {
-        console.log(`Тестирование закончено!`)
+        console.log(`Testing completed!`)
     });
 
 });
