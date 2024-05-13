b0a82a015b3db6a432f379abbb0e9153efbf8b22
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-13 08:11:12 +0300

    change declare variables from let to const in the consts.ts

--- H-W-18/Consts/consts.ts
+++ H-W-18/Consts/consts.ts
@@ -1,4 +1,4 @@
-export let expectedDataObject:
+export const expectedDataObject:
     { id: number, email: string, first_name: string, last_name: string, avatar: string } =
 {
     "id": 2,
@@ -7,25 +7,25 @@ export let expectedDataObject:
     "last_name": "Weaver",
     "avatar": "https://reqres.in/img/faces/2-image.jpg"
 }
-export let expectedSupportObject: { url: string, text: string } =
+export const expectedSupportObject: { url: string, text: string } =
 {
     "url": "https://reqres.in/#support-heading",
     "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
 };
-export let expectedPostObject: { name?: string | number, job?: string | number } = {
+export const expectedPostObject: { name?: string | number, job?: string | number } = {
     name: "Ilya",
     job: "QA"
 };
-export let newData: { name: string, job: string } = {
+export const newData: { name: string, job: string } = {
     name: "New Ilya",
     job: "New QA"
 };
-export let expectedPostObjectNumbers: { name?: string | number, job?: string | number } = {
+export const expectedPostObjectNumbers: { name?: string | number, job?: string | number } = {
     name: 1,
     job: 1
 };
 
-export let expectedObjectWhen404Error: { status?: number, message?: string } = {
+export const expectedObjectWhen404Error: { status?: number, message?: string } = {
     status: 404,
     message: 'Not Found',
 }
