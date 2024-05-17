e78ed64a68d2a0ac5a8c917bdc9d30815b12975e
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-05-17 15:40:34 +0300

    FEAT: Improved 1st test
    - Placed the constants in the separate file "consts.ts";
    - Added beforeAll and afterAll;
    - Put some of the test into the separate function inside describe.

---
+++ H-W-19/Constants/consts.ts
@@ -0,0 +1,5 @@
+export const expectedMainURL:string = "https://www.onliner.by/";
+export const catalogLinkXpath:string = "//span[text()='Каталог' and @class = 'b-main-navigation__text']";
+export const expectedCatalogURL:string = 'https://catalog.onliner.by/';
+export const expectedCatalogTitle:string = 'Каталог Onlíner';
+export const expectedCatalogHeaderTextXpath:string = "//h1[text()='Каталог' and @class = 'catalog-navigation__title']";
--- H-W-19/Tests/tests.spec.ts
+++ H-W-19/Tests/tests.spec.ts
@@ -1,27 +1,35 @@
-import  {Builder, WebDriver, By, until} from "selenium-webdriver";
+import { Builder, WebDriver, By, until } from "selenium-webdriver";
 import { describe, expect, test } from '@jest/globals';
+import { catalogLinkXpath, expectedCatalogURL, expectedCatalogTitle, expectedCatalogHeaderTextXpath, expectedMainURL} from '../Constants/consts';
 
 describe('Onliner', () => {
-    test('Move to Catalog', async () => {
-    let driver: WebDriver = await new Builder().forBrowser("chrome").build();
-    await driver.manage().window().maximize();
-    await driver.get("https://www.onliner.by/");
-    const mainURL = await driver.getCurrentUrl();
-    const mainTitle = await driver.getTitle();
-    expect(mainURL).toEqual("https://www.onliner.by/");
-    expect(mainTitle).toEqual("Onlíner");
-    const CatalogButton = await driver.findElement(By.xpath("//span[text()='Каталог' and @class = 'b-main-navigation__text']"));
-    await driver.wait(until.elementIsVisible(CatalogButton), 10000, 'Timed out after 3 sec', 1000);
-    await CatalogButton.click();
-    const catalogURL = await driver.getCurrentUrl();
-    const catalogTitle = await driver.getTitle();
-    expect(catalogURL).toEqual("https://catalog.onliner.by/");
-    expect(catalogTitle).toEqual("Каталог Onlíner");
-    const catalogText = await driver.findElement(By.xpath("//h1[text()='Каталог' and @class = 'catalog-navigation__title']"));
-    await driver.wait(until.elementIsVisible(catalogText), 10000, 'Timed out after 3 sec', 1000);
-    const catalogTextisPresent = await catalogText.isDisplayed();
-    expect(catalogTextisPresent).toEqual(true);
-    await driver.close();
-    await driver.quit();
-}, 10000);
+    let driver: WebDriver;
+    beforeAll(async () => {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.manage().window().maximize();
+        await driver.get(expectedMainURL);
     });
+
+    async function navigateToCatalog() {
+        const catalogLink = await driver.findElement(By.xpath(catalogLinkXpath));
+        await driver.wait(until.elementIsVisible(catalogLink), 10000, 'Timed out after 1 sec', 1000);
+        await driver.wait(until.elementIsEnabled(catalogLink), 10000, 'Timed out after 1 sec', 1000);
+        await catalogLink.click();
+        await driver.wait(until.urlIs(expectedCatalogURL), 10000, 'Timed out after 1 sec', 1000);
+    };
+
+    test('Move to Catalog', async () => {
+        await navigateToCatalog();
+        const catalogTitle = await driver.getTitle();
+        expect(catalogTitle).toEqual(expectedCatalogTitle);
+        const catalogHeaderText = await driver.findElement(By.xpath(expectedCatalogHeaderTextXpath));
+        await driver.wait(until.elementIsVisible(catalogHeaderText), 10000, 'Timed out after 3 sec', 1000);
+        const catalogHeaderTextIsPresent = await catalogHeaderText.isDisplayed();
+        expect(catalogHeaderTextIsPresent).toEqual(true);
+    }, 10000);
+
+    afterAll(async () => {
+        await driver.close();
+        await driver.quit();
+    });
+});
