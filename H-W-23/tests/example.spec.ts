5ed6b85fa99bea0e378641cccc1ae15f5d90c4ac
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-06-08 00:35:53 +0300

    Winston logger is added

--- H-W-23/tests/example.spec.ts
+++ H-W-23/tests/example.spec.ts
@@ -8,6 +8,7 @@ import { NewsPage } from '../Src/pageObject/newsPage';
 import { waitForUrlSEarch } from '../Src/helpers/urls';
 import { PageFactory } from '../Src/pageObject/pageFactory';
 import { only } from 'node:test';
+import logger from '../Src/helpers/logger';
 
 test.describe.configure({ mode: 'parallel', retries: 2 })
 
@@ -26,14 +27,17 @@ test.describe('3DNews.ru', () => {
     searchPage = PageFactory.getPage(page, 'Search') as SearchPage;
     newsPage = PageFactory.getPage(page, 'News') as NewsPage;
     await homePage.openPage();
+    logger.info('Test setup completed.');
   });
 
   test('Test 1: Verify social network buttons href attributes', async ({ page }) => {
     await headerPage.buttonsHasCorrectHrefAttribute();
+    logger.info('Social network buttons href attributes verified.');
   });
 
   test('Test 2: Verify displaying crypto currency', async ({ page }) => {
     await homePage.cryptoCurrencyIsVisible();
+    logger.info('Crypto currency is visible.');
   });
 
   test('Test 3: Move to Games page and verify elements', async ({ page }) => {
@@ -42,6 +46,7 @@ test.describe('3DNews.ru', () => {
     await gamesPage.titleIsCorrect(gamesPage.title);
     await gamesPage.sideBarIsVisible();
     await gamesPage.colorIsCorrect();
+    logger.info('Moved to Games page and verified elements.');
   });
 
   test('Test 4: Search on the home page and verify results', async ({ page }) => {
@@ -50,6 +55,7 @@ test.describe('3DNews.ru', () => {
     await page.waitForURL(waitForUrlSEarch);
     await searchPage.urlIsCorrect(searchPage.urlAfterSearch);
     await searchPage.titleIsCorrect(searchPage.title);
+    logger.info('Performed search and verified results.');
   });
 
   test('Test 5: Move to News page and navigate there', async ({ page }) => {
@@ -62,18 +68,22 @@ test.describe('3DNews.ru', () => {
     await clickOnButton(page, newsPage.inFutureLink);
     await newsPage.urlIsCorrect(newsPage.url);
     await newsPage.colorIsCorrect();
+    logger.info('Moved to News page and navigated, verifying URLs and colors.');
   });
 
   test('Test 6: Counting the number of reviews', async ({ page }) => {
     await countOfChildElements(homePage.reviews, 25, 'li')
+    logger.info('Counted the number of reviews.');
   });
 
   test.only('Test 7: Counting subsections of the Games section', async ({ page }) => {
     await clickOnButton(page, homePage.gamesButton);
     await countOfChildElements(gamesPage.subsectionsGames, 28, 'a')
+    logger.info('Counted subsections of the Games section.');
   });
 
   test.afterAll(async ({ browser }) => {
     await browser.close();
+    logger.info('Browser closed.');
   });
 });
