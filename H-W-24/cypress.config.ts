1a2f5c14f6117c63c15f45c6099f1b64fc45f5e1
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-06-07 16:55:59 +0300

    Allure is added

--- H-W-24/cypress.config.ts
+++ H-W-24/cypress.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from "cypress";
-
+const { allureCypress } = require("allure-cypress/reporter");
 export default defineConfig({
   e2e: {
-    supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.cy.ts",
+    supportFile: "./H-W-24/cypress/support/e2e.ts",
+    specPattern: "./H-W-24/cypress/e2e/**/*.cy.ts",
     defaultCommandTimeout: 15000,
     video: true,
-    downloadsFolder: "cypress/downloads",
-    screenshotsFolder: "cypress/screenshots",
-    fixturesFolder: "cypress/fixtures",
+    downloadsFolder: "./H-W-24/cypress/downloads",
+    screenshotsFolder: "./H-W-24/cypress/screenshots",
+    fixturesFolder: "./H-W-24/cypress/fixtures",
     screenshotOnRunFailure: true,
-    videosFolder: "cypress/videos",
+    videosFolder: "./H-W-24/cypress/videos",
     viewportHeight: 1080,
     viewportWidth: 1920,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, {
+        resultsDir: "./cypress/reports/allure-results",
+      });
+      return config;
     },
   },
 });
